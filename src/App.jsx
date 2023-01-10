898035e4be52694d1555f614294c3a5adfc04dcf
felipe-las-casas <felipelascasas7@gmail.com>
2023-01-10 12:26:40 -0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -15,23 +15,72 @@ import { useState } from 'react';
 // todo - Mostre um alerta caso o login seja efetuado com sucesso (javascript alert). Investigue a função login() para entender como ter sucesso na requisição.
 
 export default function LoginForm() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState(null)
+  const [isRequesting, setIsRequesting] = useState(false)
+
+
+  function handleEmail(event) {
+    const { value } = event.target
+    setEmail(value)
+  }
+
+  function handlePassword(event) {
+    const { value } = event.target
+    setPassword(value)
+  }
+
+
+  function handleSubmit() {
+    let values = {email: email, password: password}
+    console.log(values)
+    
+    setIsRequesting(true)
+    setError(null)
+    
+    login(values)
+    .then(() => {
+      alert('Login feito com sucesso!')
+    })
+    .catch((error) => {
+      setError(error)
+    })
+    .finally(() => {
+      setEmail('')
+      setPassword('')
+      setIsRequesting(false)
+    })
+  }
+
+
   return (
     <div className='wrapper'>
       <div className='login-form'>
         <h1>Login Form 🐞</h1>
         {/* Coloque a mensagem de erro de login na div abaixo. Mostre a div somente se houver uma mensagem de erro. */}
-        <div className='errorMessage'></div>
+        
+         {
+            error && <div className='errorMessage'>{error.message}</div>
+          
+          }
+         
         <div className='row'>
           <label htmlFor={'email'}>Email</label>
-          <input id={'email'} type={'email'} autoComplete='off' />
+          <input id={'email'} type={'email'} autoComplete='off'
+          value={email} 
+          onChange={handleEmail}/>
         </div>
         <div className='row'>
           <label htmlFor={'password'}>Password</label>
-          <input id={'password'} type={'password'} />
+          <input id={'password'} type={'password'} 
+          value={password}
+          onChange={handlePassword} />
         </div>
 
         <div className='button'>
-          <button>Login</button>
+          <button disabled={email === '' || password.length < 6 || isRequesting} 
+          onClick={handleSubmit}>Login</button>
         </div>
       </div>
     </div>
